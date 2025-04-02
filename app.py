9a2deca91ae7b2538fbd0e2a92198f2bd103d84f
Marcelo <marcelobrf3@gmail.com>
2025-04-02 16:44:52 -0300

    Corrigir Flask pra escutar em 0.0.0.0 pro Render

--- app.py
+++ app.py
@@ -290,4 +290,5 @@ def salvar_resultado():
 
 if __name__ == '__main__':
     carregar_csv()
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))  # Usa a porta do Render ou 5000 localmente
+    app.run(host="0.0.0.0", port=port, debug=True)
